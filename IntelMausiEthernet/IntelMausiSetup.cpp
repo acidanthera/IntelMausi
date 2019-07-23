d0ddb1aa47382b420bd415e29db38e6f94d18427
vit9696 <vit9696@users.noreply.github.com>
2019-07-24 02:52:41 +0300

    Provide Xcode 11 backwards compatibility with 10.14 and lower
    Reference acidanthera/bugtracker#430

--- IntelMausiEthernet/IntelMausiSetup.cpp
+++ IntelMausiEthernet/IntelMausiSetup.cpp
@@ -503,3 +503,31 @@ void IntelMausi::discardPacketFragment(bool extended)
     rxPacketHead = rxPacketTail = NULL;
     rxPacketSize = 0;
 }
+
+#ifdef __MAC_10_15
+
+/**
+ *  Ensure the symbol is not exported
+ */
+#define PRIVATE __attribute__((visibility("hidden")))
+
+/**
+ *  For private fallback symbol definition
+ */
+#define WEAKFUNC __attribute__((weak))
+
+// macOS 10.15 adds Dispatch function to all OSObject instances and basically
+// every header is now incompatible with 10.14 and earlier.
+// Here we add a stub to permit older macOS versions to link.
+// Note, this is done in both kern_util and plugin_start as plugins will not link
+// to Lilu weak exports from vtable.
+
+kern_return_t WEAKFUNC PRIVATE OSObject::Dispatch(const IORPC rpc) {
+    (panic)("OSObject::Dispatch plugin stub called");
+}
+
+kern_return_t WEAKFUNC PRIVATE OSMetaClassBase::Dispatch(const IORPC rpc) {
+    (panic)("OSMetaClassBase::Dispatch plugin stub called");
+}
+
+#endif
